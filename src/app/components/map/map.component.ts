7e5dfa60459f5ba7d806b484339621cb08de82b8
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-09-01 00:04:53 -0400

    inicializacion de mapa en map.component.ts, visualizacion en local.host

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+   // varibales - declaracion 
+   map: any;
   constructor() { }
 
-  ngOnInit() {
+
+  ngOnInit(): void {
+  
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGF0cmljaWF2YWxkZWJlbml0byIsImEiOiJjanphOHg4aGUwMXRzM29vOXozOTBycGFmIn0.eU9CSOC83F1iWUwy44ad5w';
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-96, 37.8],
+      zoom: 2
+    });
+
+    // code from the next step will go here!
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+
+     
   }
 
 }
