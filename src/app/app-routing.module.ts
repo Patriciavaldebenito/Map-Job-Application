c7bf29408a70d6dea636f16d40ba4e1700efdb18
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-08-31 23:45:23 -0400

    firt commit, project map job application, importacion modulo http en mgmodule, style css para body, link css y mapbox ql en inde.html

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//import { HomeComponent } from './components/home/home.component';
+//import { MapComponent } from './components/map/map.component';
+
+
+const routes: Routes = [
+  //{path:'', component: HomeComponent},
+ // {path:'home', component: HomeComponent},
+  //{path: 'map', component: MapComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
